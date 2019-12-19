7f00f6e466581579e98a879ac6e32f7ddaa7f122
Damely Tineo <damely.tineo07@gmail.com>
2019-12-19 05:49:50 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -18,6 +18,10 @@ def reverse_array (array)
   return array.reverse
 end
 
+def kesha_maker (array)
+  array.each {|i| i[2]= "$"}
+end
+
 def find_a (array)
   array.select { |word| word.start_with?('a') }
 end
